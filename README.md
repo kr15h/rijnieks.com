8b842275fdc88203ac9ce507174d3ce65cba23ea
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2018-01-05 14:38:42 +0100

    Start building hugo site

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # rijnieks.com
 
 Personal website GitHub repo. Now as .com
+
+Built with [GoHugo](https://gohugo.io) the static website generator.
--- ui-examples/categories/flash/vienibasbrauciens/debug.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files (x86)\Mozilla Firefox\firefox.exe" "C:\Users\Krish\Documents\_projects\cube\VienibasBrauciens\flash\bin\index.html"
