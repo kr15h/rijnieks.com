c47495a12e30caa1bae291d254c06e18f4850a2a
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2019-07-17 20:54:38 +0300

    Remove phone number and intelligent impressum

--- static/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $.ajax({
-    url: "https://tools.rijnieks.com/impressum.php", 
-    success: function(result){
-      $('#impressum').html(result);
-    }
-  });
-});
